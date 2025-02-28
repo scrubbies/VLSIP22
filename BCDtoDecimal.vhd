0a0663b2e9e2c05b3372b8d38a6a3b2aafaa390e
scrubbies <125943314+scrubbies@users.noreply.github.com>
2025-02-28 02:57:14 -0600

    Update BCDtoDecimal.v

--- BCDtoDecimal.vhd
+++ BCDtoDecimal.vhd
@@ -1,17 +1,17 @@
 module BCDtoDecimal
 (
-    input [3:0] bcd,
+    input [7:0] bcd,
     input wire reset,
-  output reg [3:0] dec
+  output reg [7:0] dec
 );
 
   always @(*) begin
     if (reset)
-	dec = 4'd0;
+	dec = 8'd0;
    else  if (bcd <= 4'd9) //making sure input is valid
             dec = bcd;
         else
-            dec = 4'd0; //0 for any invalid input
+            dec = 8'd0; //0 for any invalid input
     end
 
 endmodule
